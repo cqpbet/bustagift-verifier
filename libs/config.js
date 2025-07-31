0137809b6359460aadfc33c89eabd2bfeae88cb2
cqpdev <dev@cqp.bet>
2025-07-31 19:28:47 +0300

    We will use the hash of Bitcoin block #907975 as a salt now.

--- libs/config.js
+++ libs/config.js
@@ -2,4 +2,4 @@
 export const CHAIN_LENGTH = 1000000;
 
 // We will use the hash of Bitcoin block #907975 as a salt. At the time of this commitment, that block has not yet been mined (verifiable via commit history).
-export const PUBLIC_SECRET = "SALT_HERE";
+export const PUBLIC_SECRET = "0000000000000000000033efb7468d1962a5b487cf4613d7acee9be6474339dd";
